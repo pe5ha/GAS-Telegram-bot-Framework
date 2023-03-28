d6f0350510008d2238f54f1bbae1df7387c3265c
pe5ha <pavel.naumenko.2000@yandex.ru>
2023-03-28 23:49:19 +0300

    time util

--- Utils.js
+++ Utils.js
@@ -27,6 +27,10 @@ function stringDateDash(date,beOnlyDate=false, gmt="GMT+3"){
   }
 }
 
+function getMonthNameByDate(date){
+  return monthNames[date.getMonth()];
+}
+
 const monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
 ];
